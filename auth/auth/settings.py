9747a085e3cc7ef09bdb12e3620f6406c022d903
pruthvihosmani <67551298+pruthvihosmani@users.noreply.github.com>
2022-12-18 11:10:43 +0530

    Injest data with DRF views and mysql database

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'users',
 
 ]
@@ -79,14 +82,14 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_auth',
-        'USER': 'root',
-        'PASSWORD': 'ammaappa',
-
+        'NAME' : 'django_auth',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD': 'AmmaappA',
+        'HOST' : 'localhost',
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- auth/users/models.py
+++ auth/users/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.Charfield(max_length=255)
-    email = models.Charfield(max_length=255, unique=True)
-    password = models.Charfield(max_length=255)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
     username = None
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = []
+    REQUIRED_FIELDS = []
